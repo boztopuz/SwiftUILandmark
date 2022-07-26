56039e60e4f8c5a040d674474ccbc38d94e13b9c
BurakOztopuz <boztopuz@gmail.com>
2022-07-26 10:04:14 +0300

    Initial Commit

---
+++ SwiftUILandmark/SwiftUILandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILandmarkApp.swift
+//  SwiftUILandmark
+//
+//  Created by Burak Öztopuz on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
